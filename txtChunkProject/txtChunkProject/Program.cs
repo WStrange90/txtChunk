12856784fa9646d1f0bcb9df70a4ce0fa9ca91a5
William Strange <williamstrange90@gmail.com>
2019-08-24 18:24:45 -0400

    File separations working.

--- txtChunkProject/txtChunkProject/Program.cs
+++ txtChunkProject/txtChunkProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace txtChunkProject
 {
@@ -7,7 +8,67 @@ namespace txtChunkProject
         static void Main(string[] args)
         {
             printBanner();
-            
+
+            Console.Write("How many lines would you like the broken down files to be? ");
+            string lengthInput = Console.ReadLine();
+
+            int desiredFileLength;
+            try
+            {
+                desiredFileLength = Int32.Parse(lengthInput);
+
+                if (desiredFileLength < 1)
+                {
+                    Console.WriteLine("You entered a number smaller than 1 as your desired file length. This is not possible.");
+                    Console.WriteLine("Please enter a whole number larger than 0.");
+                    Console.WriteLine("Press enter to exit.");
+                    Environment.Exit(0);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to parse input. Are you sure you input a number?");
+                throw;
+            }
+
+            string newDocPath = "../../../Output";
+            string dropZone = @"../../../DropZone";
+            string[] dropZoneFiles = Directory.GetFiles(dropZone, "*.txt");
+            foreach (var file in dropZoneFiles)
+            {
+                string[] fileContents = System.IO.File.ReadAllLines(file);
+
+                if (desiredFileLength > fileContents.Length)
+                {
+                    Console.WriteLine("The file is not large enough to support the length you entered.");
+                    Console.WriteLine("Press enter to exit.");
+                    Environment.Exit(0);
+                }
+
+                int currentFileLine = 0;
+                int NumOfFilesMade = 0;
+
+                while (currentFileLine < fileContents.Length)
+                {
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(newDocPath, Path.GetFileNameWithoutExtension(file) + NumOfFilesMade + ".txt")))
+                    {
+                        for (int i = 0; i < desiredFileLength; i++)
+                        {
+                            outputFile.WriteLine(fileContents[currentFileLine]);
+                            currentFileLine++;
+
+                            if (currentFileLine == fileContents.Length)
+                            {
+                                break;
+                            }
+                        }
+
+                        NumOfFilesMade++;
+                    }
+                }
+                //END While
+            }
+            //END foreach file
             
         }
 
@@ -21,8 +82,6 @@ namespace txtChunkProject
             Console.WriteLine("  |   |  |   _   |  |   |    |     |_ |   _   ||       || | |   ||    _  |");
             Console.WriteLine("  |___|  |__| |__|  |___|    |_______||__| |__||_______||_|  |__||___| |_|");
             Console.WriteLine();
-            
-            Console.ReadLine();
         }
     }
 }
