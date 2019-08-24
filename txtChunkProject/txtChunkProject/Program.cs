5f103f1b93f4f94fd3528b5799294ce9aedb8211
William Strange <williamstrange90@gmail.com>
2019-08-24 04:27:55 -0400

    Created project

---
+++ txtChunkProject/txtChunkProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace txtChunkProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printBanner();
+            
+            
+        }
+
+        public static void printBanner()
+        {
+            Console.WriteLine(" _______  __   __  _______    _______  __   __  __   __  __    _  ___   _ ");
+            Console.WriteLine("|       ||  |_|  ||       |  |       ||  | |  ||  | |  ||  |  | ||   | | |");
+            Console.WriteLine("|_     _||       ||_     _|  |       ||  |_|  ||  | |  ||   |_| ||   |_| |");
+            Console.WriteLine("  |   |  |       |  |   |    |       ||       ||  |_|  ||       ||      _|");
+            Console.WriteLine("  |   |   |     |   |   |    |      _||       ||       ||  _    ||     |_ ");
+            Console.WriteLine("  |   |  |   _   |  |   |    |     |_ |   _   ||       || | |   ||    _  |");
+            Console.WriteLine("  |___|  |__| |__|  |___|    |_______||__| |__||_______||_|  |__||___| |_|");
+            Console.WriteLine();
+            
+            Console.ReadLine();
+        }
+    }
+}
